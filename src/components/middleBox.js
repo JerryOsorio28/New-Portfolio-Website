7166275c206e3d05d1798cf835313275ca8f097d
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-02-23 20:03:15 -0500

    fixed grammar in description

--- src/components/middleBox.js
+++ src/components/middleBox.js
@@ -7,8 +7,8 @@ const MiddleBox = () => {
             <h3 id='myName'>My name is Jerry Osorio Pagan</h3>
             <h1 id='description'>I am a Full Stack Web Developer from Puerto Rico.</h1>
             <p id='otherDescription'>
-            I am a Full Stack Web Developer from Lambda School, I have become very passionate about being a developer, 
-            there is this satisfying feeling in, creating out of nothing, something that can solve real-life problems and make a change, one keystroke at a time.
+            I am a Full Stack Web Developer from Lambda School, and ever since I started coding I have become very passionate about it, 
+            there is this satisfying and unique feeling in, creating out of nothing, something that can solve real-life problems and make a change, one keystroke at a time.
                 <p id='quote'>“First, solve the problem. Then, write the code.”</p>
             </p>
             <button id='contactMeButton'>Contact me</button>
